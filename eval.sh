2a19c78c73ddbad376677f5eeba9a9ad33090c22
shenhayi <11911022@mail.sustech.edu.cn>
2024-12-01 19:51:12 -0500

    fin eval 1

---
+++ eval.sh
@@ -0,0 +1,4 @@
+python main.py --split val --eval 1 --train_global 0 --train_local 0 --train_slam 0 --exp_name eval_1 \
+--load_global tmp/models/exp2/model_best.global \
+--load_local pretrained_models/model_best.local \
+--load_slam pretrained_models/model_best.slam
